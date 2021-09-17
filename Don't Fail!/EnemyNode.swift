779694bb6ab60574c7d2d3be6a4e758d502845c2
plainoaks <hiropa.440@gmail.com>
2021-09-18 00:44:02 +0900

    first Commit

--- Don't Fail!/EnemyNode.swift
+++ Don't Fail!/EnemyNode.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import SpriteKit
 
 class EnemyNode: SKSpriteNode {
+    var body: SKSpriteNode!
     var hp: CGFloat!
     let max_hp: CGFloat
     var healthBar: SKSpriteNode!
@@ -10,14 +11,15 @@ class EnemyNode: SKSpriteNode {
     var isCrazy: Bool = false
     var isDead: Bool = false
     
-    var mainWeapon: (() -> Void)!
+//    var mainWeapon: (() -> Void)!
+    var mainWeapon: Barrage!
     var attackStarted: Bool = false
     var attackEnded: Bool = false
     var lastBarrage: TimeInterval!
     var lastBullet: TimeInterval!
     var frameCount: Int
     
-    init(max_hp: CGFloat) {
+    init(scene: GameScene, max_hp: CGFloat) {
         self.max_hp = max_hp
         self.hp = max_hp
         self.lastBarrage = nil
@@ -25,11 +27,17 @@ class EnemyNode: SKSpriteNode {
         self.frameCount = 0
 
         let enemyTexture = SKTexture(imageNamed: "enemy")
-        super.init(texture: enemyTexture, color: .clear, size: enemyTexture.size())
+        super.init(texture: nil, color: .clear, size: enemyTexture.size())
+        
+        self.body = SKSpriteNode(imageNamed: "enemy")
+        addChild(self.body)
+        let rotate = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: 6.0)
+        self.body.run(SKAction.repeatForever(rotate))
         
         self.healthBar = SKSpriteNode(color: .blue, size: CGSize(width: self.size.width, height: 5))
         self.healthBar.anchorPoint = CGPoint(x: 0.0, y: 0.5)
         self.healthBar.position = CGPoint(x: -self.size.width * 0.5, y: self.size.height * 0.5 + 15)
+        self.healthBar.zRotation = 0
         addChild(self.healthBar)
         let healthBarBackground = SKSpriteNode(imageNamed: "healthBarBackground")
         healthBarBackground.anchorPoint = CGPoint(x: 0.0, y: 0.5)
